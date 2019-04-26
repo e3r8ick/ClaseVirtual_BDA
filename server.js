1b8ff6497803519621b44c19886ec2e80cf6bbf2
Erick Cordero <eguicoro2@gmail.com>
2019-04-26 14:05:01 -0600

    server setteted in port 3000

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+//require
+var express = require("express");
+var bodyParser = require("body-parser");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/RentACar";
 
-MongoClient.connect(url, function(err, db) {
+//server
+var app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//host
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("App escuchando en http://%s:%s",host,port);
+})
+
+MongoClient.connect(url,{ useNewUrlParser: true }, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   db.close();
